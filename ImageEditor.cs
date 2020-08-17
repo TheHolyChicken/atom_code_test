33894a2243c462a701dda0e0f089e32e4ed7b84b
Colin Pearce <colindpearce@gmail.com>
2020-08-17 12:01:54 +0100

    Refactor width/height uints into a single Size value

--- ImageEditor.cs
+++ ImageEditor.cs
@@ -8,26 +8,27 @@ class ImageEditor
         png,
         jpg
     }
-    private static bool dimensionsAreValid(uint width, uint height) {
+    private static bool dimensionsAreValid(System.Drawing.Size size) {
+        // max permissible size is 4k resolution
         const uint max_resolution_width = 3840;
         const uint max_resolution_height = 2160;
 
-        bool width_valid = (width > 0 && width <= max_resolution_width);
-        bool height_valid = (height > 0 && height <= max_resolution_height);
+        bool width_valid = (size.Width > 0 && size.Width <= max_resolution_width);
+        bool height_valid = (size.Height > 0 && size.Height <= max_resolution_height);
         return width_valid && height_valid;
     }
 
-    private static string cachedImagePath(string path, uint width, uint height, ImageFormat format, string watermark, Color background) {
+    private static string cachedImagePath(string path, System.Drawing.Size size, ImageFormat format, string watermark, Color background) {
         // TODO: implement cache, and return a valid string pointing to the cached image if it exists
         return "";
     }
 
-    public static void getImage(string path, uint width, uint height, ImageFormat format, string watermark = "", Color background = default(Color)) {
-        if (!File.Exists(path) || !dimensionsAreValid(width, height)) {
+    public static void getImage(string path, System.Drawing.Size size, ImageFormat format, string watermark = "", Color background = default(Color)) {
+        if (!File.Exists(path) || !dimensionsAreValid(size)) {
             return;
         }
 
-        string cached_path = cachedImagePath(path, width, height, format, watermark, background);
+        string cached_path = cachedImagePath(path, size, format, watermark, background);
         if (File.Exists(cached_path)) {
             // use cached image file
         }
