8b125c635e5ad7a2c3ed562a2b18a1f2a0bf7e95
Colin Pearce <colindpearce@gmail.com>
2020-08-20 10:06:00 +0100

    Implement simple dictionary-based file cache

--- IImageCache_Dummy.cs
+++ IImageCache_Dummy.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 
@@ -9,22 +10,25 @@ namespace ImageCache {
         {
             CacheFolder = cache_folder;
             Directory.CreateDirectory(CacheFolder);
+            stored_images_ = new Dictionary<ImageProperties, string>();
         }
 
         public string cachedImagePath(ref ImageProperties desired_image)
         {
-            // stub implementaiton
-            // Here is where we *would* check if an image matching the arguments had already been generated and cached, and return the path to that image
-            return "";
+            string cached_image_path;
+            stored_images_.TryGetValue(desired_image, out cached_image_path);
+            return cached_image_path;
         }
 
         public void saveImageToCache(Image image, ref ImageProperties image_properties)
         {
-            string cache_image_name = "test";
+            string cache_image_name = stored_images_.Count.ToString();
             string cache_image_path = CacheFolder + cache_image_name + "." + image_properties.Format.ToString();
             image.Save(cache_image_path, image_properties.Format);
+            stored_images_.TryAdd(image_properties, cache_image_path);
         }
 
         public string CacheFolder { get; }
+        private Dictionary<ImageProperties, string> stored_images_;
     }
 }
